493a4e7e70997a8c3adee4f0f983f60d7575b74e
dani quinn sexton <spells@witch.zone>
2019-10-12 20:50:49 +0100

    add rollup to build library

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from 'rollup-plugin-typescript2'
+
+export default {
+  input: './src/index.ts',
+  output: {
+    name: 'nodecg-twitchie-graphics',
+    file: './lib/nodecg-twitchie-graphics.js',
+    format: 'esm',
+    sourcemap: true,
+  },
+  external: ['nodecg-twitchie'],
+  plugins: [typescript()],
+}
